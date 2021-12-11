33cd70049fb3dfd2dd040e869e685274caa62a4a
Haziq Hairil <haziqhairil21@gmail.com>
2021-12-11 23:41:33 +0800

    Dummy litter stats

--- src/app.py
+++ src/app.py
@@ -3,49 +3,38 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Placeholder for database stuff
+dummy_bins = [
+    {
+        'id': i,
+        'img_path': 'bins/0.jpg',
+        'address': '209 Bishan Street 23',
+        'location': 'Staircase 5A'
+    }
+    for i in range(10)
+]
+
 
 @app.route('/')
 def index():
-    # Placeholder for database stuff
-    dummy_bins = [
-        {
-            'img_path': 'bins/0.jpg',
-            'address': '209 Bishan Street 23',
-            'location': 'Staircase 5A'
-        },
-        {
-            'img_path': 'bins/0.jpg',
-            'address': '209 Bishan Street 23',
-            'location': 'Staircase 5A'
-        },
-        {
-            'img_path': 'bins/0.jpg',
-            'address': '209 Bishan Street 23',
-            'location': 'Staircase 5A'
-        },
-        {
-            'img_path': 'bins/0.jpg',
-            'address': '209 Bishan Street 23',
-            'location': 'Staircase 5A'
-        },
-        {
-            'img_path': 'bins/0.jpg',
-            'address': '209 Bishan Street 23',
-            'location': 'Staircase 5A'
-        },
-        {
-            'img_path': 'bins/0.jpg',
-            'address': '209 Bishan Street 23',
-            'location': 'Staircase 5A'
-        },
-    ]
-
     return render_template('base.html', bins=dummy_bins)
 
 
-@app.route('/<id>')
+@app.route('/<int:id>')
 def garbage_stats(id):
-    return render_template('garbage-stats.html', id=id)
+    # Placeholder for SQLite stuff
+    bin_data = [
+        # (time, litter_count)
+        (0.,    0),
+        (600.,  2),
+        (1200., 5),
+        (1800., 8),
+        (2400., 8),
+        (3000., 12),
+        (3600., 1)
+    ]
+
+    return render_template('garbage-stats.html', bins=dummy_bins, target_bin=bin_data)
 
 
 if __name__ == "__main__":
