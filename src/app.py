2609af752dd6343e62984fcd7099b11664fa100e
Haziq Hairil <haziqhairil21@gmail.com>
2021-12-11 19:22:49 +0800

    Homepage route

--- src/app.py
+++ src/app.py
@@ -4,9 +4,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/<id>")
-def index(id):
-    return render_template("index.html", id=id)
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+
+@app.route('/<id>')
+def garbage_stats(id):
+    return render_template('garbage-stats.html', id=id)
 
 
 if __name__ == "__main__":
