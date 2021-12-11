6fdd44642446470d901326c1f33023fcc84ec719
Haziq Hairil <haziqhairil21@gmail.com>
2021-12-11 18:45:31 +0800

    Rearrange files

--- src/app.py
+++ src/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
+
 
 @app.route("/<id>")
 def index(id):
     return render_template("index.html", id=id)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
