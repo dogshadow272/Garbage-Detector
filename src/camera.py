cfca9cddc41fc0c05062764e846a604bab45f655
Haziq Hairil <haziqhairil21@gmail.com>
2021-12-13 18:33:12 +0800

    Bugfix for camera module

--- src/camera.py
+++ src/camera.py
@@ -1,35 +1,31 @@
 import json
 import cv2
-from time import time, sleep #imports
+from time import time, sleep
+
 
 def photo_capture():
-    
     cam = cv2.VideoCapture(0)
     img_counter = 0
-    
+
     while True:
         ret, frame = cam.read()
+
         if not ret:
             print("failed to grab frame")
             break
-        k = cv2.waitKey(1)
-        if k%256 == 27:                         # quits program it escape key is pressed
-            # ESC pressed
-            print("Escape hit, closing...")
-            break
-        
-        while True:
-            # SPACE pressed
-            img_name = "opencv_frame_{}.png".format(img_counter) #saves photo in the directory
-            cv2.imwrite(img_name, frame)
-            print("{} written!".format(img_name))
-            img_counter += 1
 
-            sleep(60 - time() % 60)
-    
+        # Save photo in the directory
+        img_path = f'opencv_frame_{img_counter}.png'
+        cv2.imwrite(img_path, frame)
+        print(f'{img_path} written!')
+        img_counter += 1
+
+        # Take a picture every minute, ignoring the
+        # time it takes to execute the rest of the code
+        sleep(60 - time() % 60)
+
     cam.release()
-    
     cv2.destroyAllWindows()
-        
+
 
 photo_capture()
