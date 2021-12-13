2ac0615c712e6c67904a902bf573d194596af363
Haziq Hairil <haziqhairil21@gmail.com>
2021-12-13 13:34:55 +0800

    Fix typo

--- src/app.py
+++ src/app.py
@@ -7,13 +7,16 @@ def get_columns(table):
     '''Get columns of table (str) and return a list'''
     return [i[1] for i in cur.execute(f'PRAGMA table_info({table})').fetchall()]
 
+
 def get_property(id, property):
     '''Get property (str) of bin with id (int) and return a string'''
     return cur.execute(f'SELECT {property} FROM info WHERE id={id}').fetchone()[0]
 
+
 def new_entry(data):
     '''Inserts data (list) into SQL database db.db'''
-    cur.execute(f'INSERT INTO data VALUES ({time.time()}, {", ".join(map(str, data))})')
+    cur.execute(
+        f'INSERT INTO data VALUES ({time.time()}, {", ".join(map(str, data))})')
 
 
 app = Flask(__name__)
@@ -39,10 +42,7 @@ def garbage_stats(id):
     litter_counts = [i[0] for i in cur.execute(f'SELECT bin{id} FROM data')]
     litter_data = (time_stamps, litter_counts)
 
-    print(dummy_bins)
-    print(litter_data)
-
-    return render_template('garbage-stats.html', bins=dummy_bins, target=id, litter_data=dummy_data)
+    return render_template('garbage-stats.html', bins=dummy_bins, target=id, litter_data=litter_data)
 
 
 if __name__ == "__main__":
