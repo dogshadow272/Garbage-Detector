3c5472c23dca000500624c44b6d269c8b7bc5bf9
Shivreshi <shivreshi@gmail.com>
2021-12-18 12:56:56 +0000

    camera.py

--- src/camera.py
+++ src/camera.py
@@ -1,31 +1,30 @@
-import json
 import cv2
-from time import time, sleep
+from time import sleep
+import requests
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("test")
+
+img_counter = 0
+camera_number = 1
 
 
-def photo_capture():
-    cam = cv2.VideoCapture(0)
-    img_counter = 0
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    sleep(6)
+    img_name = f"{camera_number}.png"
+    cv2.imwrite(img_name, frame)
+    # img_counter += 1
 
-    while True:
-        ret, frame = cam.read()
+cam.release()
 
-        if not ret:
-            print('failed to grab frame')
-            break
-
-        # Save photo in the directory
-        img_path = f'opencv_frame_{img_counter}.png'
-        cv2.imwrite(img_path, frame)
-        print(f'{img_path} written!')
-        img_counter += 1
-
-        # Take a picture every minute, ignoring the
-        # time it takes to execute the rest of the code
-        sleep(60 - time() % 60)
-
-    cam.release()
-    cv2.destroyAllWindows()
+cv2.destroyAllWindows()
 
 
-photo_capture()
+data = {"img_name": f"{camera_number}.png"}
+url = f"https://ide-37415cdf43eb4b2aa570be45f9eb632e-8080.cs50.ws/{camera_number}"
+response = requests.post(url, data)
+print(response)
