f84b2fa9c90c387b9350e30e9b69d70a864f27f4
Haziq Hairil <haziqhairil21@gmail.com>
2022-01-29 17:19:20 +0800

    Remove redundant code from `sns.py`

--- src/sns.py
+++ src/sns.py
@@ -1,28 +1,10 @@
 import boto3
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
+client = boto3.client('sns', region_name='us-east-1')
 
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_KEY_ID')
-AWS_SESSION_TOKEN = os.getenv('AWS_SESSION_TOKEN')
-TOPIC_ARN = '' # add TopicArn here
-
-client = boto3.client(
-    'sns',
-    AWS_ACCESS_KEY_ID=AWS_ACCESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY=AWS_SECRET_ACCESS_KEY,
-    AWS_SESSION_TOKEN=AWS_SESSION_TOKEN,
-    region_name='us-east-1'
-)
 
 def send_message(message):
-    response = client.publish(
-        TopicArn=TOPIC_ARN,
+    client.publish(
+        TopicArn='add topic arn here',
         Message=message
     )
-    return response
-
-if __name__ == '__main__':
-    pass
