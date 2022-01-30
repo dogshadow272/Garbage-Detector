8523d4153bdd0f62a8132922f5cdb1d69b0beaef
Haziq Hairil <haziqhairil21@gmail.com>
2022-01-30 12:18:40 +0800

    Add SNS ARN

--- src/sns.py
+++ src/sns.py
@@ -5,6 +5,6 @@ client = boto3.client('sns', region_name='us-east-1')
 
 def send_message(message):
     client.publish(
-        TopicArn='add topic arn here',
+        TopicArn='arn:aws:sns:us-east-1:126728685550:garbage-detect',
         Message=message
     )
